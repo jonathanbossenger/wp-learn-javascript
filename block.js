a7404efefb53573da11f189631b5e05239aae325
Jonathan Bossenger <jonathanbossenger@gmail.com>
2022-11-15 17:04:05 +0200

    Adding attributes and RichText support

--- block.js
+++ block.js
@@ -1,14 +1,34 @@
-( function ( blocks, element, blockEditor ) {
+(function (blocks, element, blockEditor) {
     var el = element.createElement;
+    var RichText = blockEditor.RichText;
+    var useBlockProps = blockEditor.useBlockProps;
 
-    blocks.registerBlockType( 'wp-learn-javascript/javascript-block', {
-        edit: function () {
-            var blockProps = blockEditor.useBlockProps();
-            return el( 'p', blockProps, 'Hello JavaScript World (from the editor).' );
+    blocks.registerBlockType('wp-learn-javascript/javascript-block', {
+        edit: function ({attributes, setAttributes}) {
+            var blockProps = useBlockProps();
+
+            function onChangeContent(newContent) {
+                setAttributes({content: newContent});
+            }
+
+            return el(
+                RichText,
+                Object.assign(blockProps, {
+                    tagName: 'p',
+                    onChange: onChangeContent,
+                    value: attributes.content,
+                })
+            );
         },
-        save: function () {
-            var blockProps = blockEditor.useBlockProps.save();
-            return el( 'p', blockProps, 'Hello JavaScript World (from the frontend).' );
+        save: function ({attributes}) {
+            var blockProps = useBlockProps.save();
+            return el(
+                RichText.Content,
+                Object.assign(blockProps, {
+                    tagName: 'p',
+                    value: attributes.content,
+                })
+            );
         },
-    } );
-} )( window.wp.blocks, window.wp.element, window.wp.blockEditor );
+    });
+})(window.wp.blocks, window.wp.element, window.wp.blockEditor);
