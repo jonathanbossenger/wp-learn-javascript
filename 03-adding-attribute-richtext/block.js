2ecf73ab4473ddeee2eb260e31c8d4ed20437678
Jonathan Bossenger <jonathanbossenger@gmail.com>
2022-11-16 12:26:53 +0200

    Adding 03-adding-attribute-richtext

---
+++ 03-adding-attribute-richtext/block.js
@@ -0,0 +1,32 @@
+( function ( blocks, element, blockEditor ) {
+    var el = element.createElement;
+    var RichText = blockEditor.RichText;
+    var useBlockProps = blockEditor.useBlockProps;
+
+    blocks.registerBlockType( 'wp-learn-javascript/javascript-block', {
+        edit: function ( { attributes, setAttributes } ) {
+            var blockProps = useBlockProps();
+            function onChangeContent( newContent ) {
+                setAttributes( { content: newContent } );
+            }
+            return el(
+                RichText,
+                Object.assign( blockProps, {
+                    tagName: 'p',
+                    onChange: onChangeContent,
+                    value: attributes.content,
+                } )
+            );
+        },
+        save: function (props) {
+            var blockProps = useBlockProps.save();
+            return el(
+                RichText.Content,
+                Object.assign( blockProps, {
+                    tagName: 'p',
+                    value: props.attributes.content,
+                } )
+            );
+        },
+    } );
+} )( window.wp.blocks, window.wp.element, window.wp.blockEditor );
